72a1aa4b71c6f3e97941a14dff63712c2c0020ee
ranmee@gmail.com <Fibs12^12c>
2016-12-03 19:55:56 +0200

    Added the main activity of the app

--- TAUSurveryApp/app/src/main/java/tau/user/tausurveryapp/MainActivity.java
+++ TAUSurveryApp/app/src/main/java/tau/user/tausurveryapp/MainActivity.java
@@ -1,5 +1,7 @@
 package tau.user.tausurveryapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the preferences (key-value pairs) of this activity.
+        SharedPreferences prefs = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        boolean isRegistered = prefs.getBoolean(getString(R.string.key_is_registered), false);
+
+        if (!isRegistered) {
+            // TODO: go to registration activity
+        }
+        else {
+            // TODO: go to main info activity
+        }
     }
 }
