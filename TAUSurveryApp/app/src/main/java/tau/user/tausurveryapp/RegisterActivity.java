4bf9696f8acc8e0c9c947c3e4197500f7e4c0392
tjg0010 <geller_tal@yahoo.com>
2016-12-10 16:30:44 +0200

    Added fields to registrey screen

--- TAUSurveryApp/app/src/main/java/tau/user/tausurveryapp/RegisterActivity.java
+++ TAUSurveryApp/app/src/main/java/tau/user/tausurveryapp/RegisterActivity.java
@@ -1,5 +1,6 @@
 package tau.user.tausurveryapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ActionBar actionBar = getSupportActionBar();
+        setTitle("טופס הרשמה");
+        actionBar.setHomeButtonEnabled(true);
+//        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
